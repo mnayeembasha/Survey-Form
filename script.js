6cb377a28240a6f5c2a70e037876a2b66c10259c
mnayeembasha <mnayeembasha@gmail.com>
2024-02-22 17:57:29 +0530

    Updated Styling

---
+++ script.js
@@ -0,0 +1,10 @@
+let inputBox = document.querySelectorAll("input");
+
+for (let input of inputBox) {
+    input.addEventListener("mouseout",(event)=>{
+        if(event.target.value){
+            event.target.style.backgroundColor="#e8f0fe";
+        }
+    })
+}
+
